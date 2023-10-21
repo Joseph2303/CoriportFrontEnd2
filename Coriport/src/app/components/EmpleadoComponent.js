478c72ed6370603b414550e6ed7405920b505a7f
Quintanilla1299 <suke12k17@gmail.com>
2023-10-20 20:41:38 -0600

    result sprint1

---
+++ Coriport/src/app/components/EmpleadoComponent.js
@@ -0,0 +1,83 @@
+
+    function createEmpleado(event) {
+        event.preventDefault();
+
+        let empleadoData = {
+            "nombre": $("#nombre").val(),
+            "apellido1": $("#apellido1").val(),
+            "apellido2": $("#apellido2").val(),
+            "telefono1": $("#telefono1").val(),
+            "telefono2": $("#telefono2").val(),
+            "cedula": $("#cedula").val(),
+            "fechaContrat": $("#fechaContrat").val(),
+            "idUsuario": $("#idUsuario").val(),
+            "idPuesto": $("#idPuesto").val()
+        };
+
+        let data = 'data=' + JSON.stringify(empleadoData);
+
+        $.ajax({
+            url: "http://localhost:8000/api/empleado/store", 
+            type: "POST",
+            data: data
+        }).done(function (response) {
+            console.log(response);
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
+
+    function deleteEmpleado(event) {
+        event.preventDefault();
+
+        let id = $("#employeeId").val(); 
+        $.ajax({
+            url: "http://localhost:8000/api/empleado/delete/" + id,
+            type: "DELETE",
+        }).done(function (response) {
+            console.log(response);
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
+
+    function findEmpleado(event) {
+        event.preventDefault();
+
+        let id = $("#searchEmpleadoId").val(); 
+        $.ajax({
+            url: "http://localhost:8000/api/empleado/show/" + id, 
+            type: "GET",
+        }).done(function (response) {
+            console.log(response);
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
+
+    function updateEmpleado(event) {
+        event.preventDefault();
+
+        let id = $("#empleadoId").val(); 
+        let updatedEmployeeData = {
+        "nombre": $("#newNombre").val(),
+            "apellido1": $("#newApellido1").val(),
+            "apellido2": $("#newApellido2").val(),
+            "telefono1": $("#newTelefono1").val(),
+            "telefono2": $("#newTelefono2").val(),
+            "cedula": $("#newCedula").val(),
+            "fechaContrat": $("#newFechaContrat").val(),
+        };
+
+        let data = 'data=' + JSON.stringify(updatedEmployeeData);
+
+        $.ajax({
+            url: "http://localhost:8000/api/empleado/update/" + id,
+            type: "PUT", 
+            data: data
+        }).done(function (response) {
+            console.log(response);
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
--- Coriport/src/app/components/UserComponent.js
+++ Coriport/src/app/components/UserComponent.js
@@ -5,7 +5,7 @@ function send(event){
 
         "email":$("#email").val(),
         "contrasena":$("#contrasena").val(),
-        "tipoUsuario":"empleado"
+        "tipoUsuario":$("#tipoUsuario").val(),
     }
     let data ='data=' + JSON.stringify(obj);
     $.ajax({
@@ -14,8 +14,79 @@ function send(event){
         data:data
     }).done(function(response){
         console.log(response);
+        localStorage.setItem('id', response.data.id);
+        
     }).fail(function(error){
         console.log(error)
     });
 }
+
+function destroy(id){
+   // let id = $("#id").val() 
+    $.ajax({
+        url:"http://localhost:8000/api/user/delete/" + id,
+        type:"DELETE",
+    }).done(function(response){
+        console.log(response);
+    }).fail(function(error){
+        console.log(error)
+    });
+}
+
+function find(event){
+    event.preventDefault(); 
+
+
+}
+
+function update(id){
+    let obj={
+        "id": id,
+        "email":$("#email").val(),
+        "contrasena":$("#contrasena").val(),
+        "tipoUsuario":$("#tipoUsuario").val(),
+    }
+
+    let data = 'data=' + JSON.stringify(obj);
+    console.log(id)
+    console.log(data)
+    $.ajax({
+        url: "http://localhost:8000/api/user/update/" + id,
+        type: "PUT", 
+        data: data
+    }).done(function (response) {
+        console.log(response);
+        window.location.href ="http://127.0.0.1:5500//Coriport/src/app/views/Encargado/MenuEncargado.html";
+
+    }).fail(function (error) {
+        console.log(error);
+        console.log(data)
+    });
+}
+
+event.preventDefault(); 
+$.ajax({
+    url:"http://localhost:8000/api/users",
+    type:"GET"
+ }).done(function(response){
+     console.log(response);
+     var respObj=response.data;
+     for(k in respObj){
+         $("#dataTable").append(
+             `<tr data-user-id="${respObj[k].id}">
+             <td >`+respObj[k].id+`</td>
+             <td>`+respObj[k].email+`</td>
+             <td>`+respObj[k].contrasena+`</td>
+             <td>`+respObj[k].tipoUsuario+`</td>
+             <td><input type="checkbox"></td>
+             </tr>`
+         );
+     }    
+ }).fail(function(){
+     
+ });
+
+
 $("#send").click(send);
+$("#destroy").click(destroy);
+$("#update").click(update);
