e370b9f0c0d13c7a98c1cd0e6117d0e26d2e103c
Joseph2303 <106033045+Joseph2303@users.noreply.github.com>
2023-11-07 23:43:24 -0600

    caso 15

--- Coriport/src/app/Scripts/ScriptEncargado/Usuario.js
+++ Coriport/src/app/Scripts/ScriptEncargado/Usuario.js
@@ -20,6 +20,8 @@ function actualizarBotones() {
         actualizarBtn.style.display = "none";
         eliminarBtn.style.display = "none";
     }
+
+    
 }
 
 // Event listener para checkboxes
@@ -43,3 +45,49 @@ document.getElementById("eliminar-seleccionados").addEventListener("click", func
 document.getElementById("actualizar-seleccionados").addEventListener("click", function() {
     // Tu lógica de actualización aquí
 });
+
+
+
+function actualizarDatos(button) {
+    var row = button.parentNode.parentNode;
+    var cells = row.getElementsByTagName("td");
+
+    document.getElementById("name").value = cells[4].textContent;
+    document.getElementById("first_lastname").value = cells[5].textContent;
+    document.getElementById("second_lastname").value = cells[6].textContent;
+    document.getElementById("first_phone").value = cells[7].textContent;
+    document.getElementById("second_phone").value = cells[8].textContent;
+    document.getElementById("cedula").value = cells[9].textContent;
+    document.getElementById("email").value = cells[1].textContent;
+    document.getElementById("password").value = cells[2].textContent;
+    document.getElementById("user_type").value = cells[3].textContent;
+    document.getElementById("user_id").value = cells[11].textContent;
+    document.getElementById("hire_date").value = cells[10].textContent;
+
+    var positionSelect = document.getElementById("position");
+    var positionValue = cells[12].textContent;
+    for (var i = 0; i < positionSelect.options.length; i++) {
+        if (positionSelect.options[i].text === positionValue) {
+            positionSelect.selectedIndex = i;
+            break;
+        }
+    }
+
+    document.getElementById("formulario-emergente").style.display = "block";
+}
+
+
+function limpiarFormulario() {
+    document.getElementById("name").value = "";
+    document.getElementById("first_lastname").value = "";
+    document.getElementById("second_lastname").value = "";
+    document.getElementById("first_phone").value = "";
+    document.getElementById("second_phone").value = "";
+    document.getElementById("cedula").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("user_type").value = "";
+    document.getElementById("user_id").value = "";
+    document.getElementById("hire_date").value = "";
+    document.getElementById("position").selectedIndex = 0;
+}
