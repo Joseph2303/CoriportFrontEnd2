8c1e746a650ff755272a028d9ea4a3f55fa2f93e
Quintanilla1299 <suke12k17@gmail.com>
2023-11-12 21:33:01 -0600

    .
    Co-authored-by: JOSEPH ALVAREZ RUIZ <joseph.alvarez.ruiz@est.una.ac.cr>

--- Coriport/src/app/components/EmpleadoComponent.js
+++ Coriport/src/app/components/EmpleadoComponent.js
@@ -1,83 +1,88 @@
+function send(event) {
+    event.preventDefault();
 
-    function createEmpleado(event) {
-        event.preventDefault();
+    let empleadoData = {
+        "nombre": $("#nombre").val(),
+        "apellido1": $("#apellido1").val(),
+        "apellido2": $("#apellido2").val(),
+        "telefono1": $("#telefono1").val(),
+        "telefono2": $("#telefono2").val(),
+        "cedula": $("#cedula").val(),
+        "fechaContrat": $("#fechaContrat").val(),
+        "idUsuario": $("#idUsuario").val(),
+        "idPuesto": $("#idPuesto").val()
+    };
 
-        let empleadoData = {
-            "nombre": $("#nombre").val(),
-            "apellido1": $("#apellido1").val(),
-            "apellido2": $("#apellido2").val(),
-            "telefono1": $("#telefono1").val(),
-            "telefono2": $("#telefono2").val(),
-            "cedula": $("#cedula").val(),
-            "fechaContrat": $("#fechaContrat").val(),
-            "idUsuario": $("#idUsuario").val(),
-            "idPuesto": $("#idPuesto").val()
-        };
+    let data = 'data=' + JSON.stringify(empleadoData);
 
-        let data = 'data=' + JSON.stringify(empleadoData);
+    $.ajax({
+        url: "http://localhost:8000/api/empleado/store",
+        type: "POST",
+        data: data
 
-        $.ajax({
-            url: "http://localhost:8000/api/empleado/store", 
-            type: "POST",
-            data: data
-        }).done(function (response) {
-            console.log(response);
-        }).fail(function (error) {
-            console.log(error);
-        });
-    }
+    }).done(function (response) {
+        console.log(response);
+        localStorage.setItem('Empleado', data);
+    }).fail(function (error) {
+        console.log(error);
+    });
+}
 
-    function deleteEmpleado(event) {
-        event.preventDefault();
+function deleteEmpleado(event) {
+    event.preventDefault();
 
-        let id = $("#employeeId").val(); 
-        $.ajax({
-            url: "http://localhost:8000/api/empleado/delete/" + id,
-            type: "DELETE",
-        }).done(function (response) {
-            console.log(response);
-        }).fail(function (error) {
-            console.log(error);
-        });
-    }
+    let id = $("#employeeId").val();
+    $.ajax({
+        url: "http://localhost:8000/api/empleado/delete/" + id,
+        type: "DELETE",
+    }).done(function (response) {
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error);
+    });
+}
 
-    function findEmpleado(event) {
-        event.preventDefault();
+function findEmpleado(event) {
+    event.preventDefault();
 
-        let id = $("#searchEmpleadoId").val(); 
-        $.ajax({
-            url: "http://localhost:8000/api/empleado/show/" + id, 
-            type: "GET",
-        }).done(function (response) {
-            console.log(response);
-        }).fail(function (error) {
-            console.log(error);
-        });
-    }
+    let id = $("#searchEmpleadoId").val();
+    $.ajax({
+        url: "http://localhost:8000/api/empleado/show/" + id,
+        type: "GET",
+    }).done(function (response) {
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error);
+    });
+}
 
-    function updateEmpleado(event) {
-        event.preventDefault();
+function updateEmpleado(event) {
+    event.preventDefault();
 
-        let id = $("#empleadoId").val(); 
-        let updatedEmployeeData = {
+    let id = $("#empleadoId").val();
+    let updatedEmployeeData = {
         "nombre": $("#newNombre").val(),
-            "apellido1": $("#newApellido1").val(),
-            "apellido2": $("#newApellido2").val(),
-            "telefono1": $("#newTelefono1").val(),
-            "telefono2": $("#newTelefono2").val(),
-            "cedula": $("#newCedula").val(),
-            "fechaContrat": $("#newFechaContrat").val(),
-        };
+        "apellido1": $("#newApellido1").val(),
+        "apellido2": $("#newApellido2").val(),
+        "telefono1": $("#newTelefono1").val(),
+        "telefono2": $("#newTelefono2").val(),
+        "cedula": $("#newCedula").val(),
+        "fechaContrat": $("#newFechaContrat").val(),
+    };
 
-        let data = 'data=' + JSON.stringify(updatedEmployeeData);
+    let data = 'data=' + JSON.stringify(updatedEmployeeData);
 
-        $.ajax({
-            url: "http://localhost:8000/api/empleado/update/" + id,
-            type: "PUT", 
-            data: data
-        }).done(function (response) {
-            console.log(response);
-        }).fail(function (error) {
-            console.log(error);
-        });
-    }
+    $.ajax({
+        url: "http://localhost:8000/api/empleado/update/" + id,
+        type: "PUT",
+        data: data
+    }).done(function (response) {
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error);
+    });
+}
+
+$("#send").click(send);
+$("#destroy").click(destroy);
+$("#update").click(update);
