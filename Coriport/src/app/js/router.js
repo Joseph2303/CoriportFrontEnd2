ffbe05a3e3ec2069bf38ec4a1a1a24258a49390d
Joseph2303 <106033045+Joseph2303@users.noreply.github.com>
2024-02-22 15:41:53 -0600

    caso 50

--- Coriport/src/app/js/router.js
+++ Coriport/src/app/js/router.js
@@ -1,6 +1,13 @@
 
 $("#diasFeriado").on('click',function(){routingi("feriado")});
 $("#empleados").on('click',function(){routingi("empleado")});
+$("#registroTardia").on('click',function(){routingi("registroTardia")});
+$("#justiTardia").on('click',function(){routingi("justiTardia")});
+$("#registroAusencia").on('click',function(){routingi("registroAusencia")});
+$("#justiAusencia").on('click',function(){routingi("justiAusencia")});
+$("#horario").on('click',function(){routingi("horario")});
+$("#marcas").on('click',function(){routingi("marcas")});
+
 
 function routingi(router){
     $('#main-containerEncargado').load('/Coriport/src/app/views/Encargado/'+router+'.html');
