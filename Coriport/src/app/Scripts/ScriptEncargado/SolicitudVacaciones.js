e23f86528f1c9f34f136af730e3f48bf883e0464
Quintanilla1299 <suke12k17@gmail.com>
2024-03-10 15:38:36 -0600

    caso 21

---
+++ Coriport/src/app/Scripts/ScriptEncargado/SolicitudVacaciones.js
@@ -0,0 +1,13 @@
+tabla.addEventListener('change', function (event) {
+    let seleccionados = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (event.target.type === 'checkbox') {
+        if (seleccionados.length > 1) {
+            event.target.checked = false;
+
+            alert('¡Solo puede seleccionar un empleado a la vez!')
+        } else {
+
+
+        }
+    }
+});
