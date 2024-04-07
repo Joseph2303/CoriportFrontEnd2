b55b14aa26ebb5d5e37812f3e8982662b467bb47
Quintanilla1299 <suke12k17@gmail.com>
2024-04-07 14:22:41 -0600

    caso 21

---
+++ Coriport/src/app/components/empleado/RegistroAusenciaComponent.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    cargarTabla();
+});
+
+
+function cargarTabla() {
+    $.ajax({
+        url: "http://localhost:8000/api/registroAusencias",
+        type: "GET"
+    }).done(function (response) {
+
+        $("#dataTableRA").empty(); // Vaciar la tabla antes de cargar los nuevos datos
+        var respObj = response.data;
+        for (k in respObj) { 
+
+            let justificacion = respObj[k].idJustificacionAusencia ? "Justificado" : "Sin Justificar";
+            let filaHTML = `<tr data-employee-id="${respObj[k].empleado.idEmpleado}" data-justificacion="${encodeURIComponent(JSON.stringify(respObj[k].justificacion_ausencia))}" data-empleado="${encodeURIComponent(JSON.stringify(respObj[k].empleado))}">
+                <td >${respObj[k].idRegistroAusencia}</td>
+                <td>${respObj[k].fecha}</td>
+                <td id="empleado">${respObj[k].empleado.nombre}</td>
+                <td id="justificacion">${justificacion}</td>
+                <td><input type="checkbox" class="checkbox-accion" onchange=""></td>
+            </tr>`;
+
+            let fila = $(filaHTML);
+            $("#dataTableRA").append(fila);
+        }
+    }).fail(function (error) {
+        console.log(error)
+    });
+
+
+}
+
---
+++ Coriport/src/app/components/empleado/RegistroTardiaComponent.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    cargarTabla();
+});
+
+function cargarTabla() {
+    $.ajax({
+        url: "http://localhost:8000/api/registroTardias",
+        type: "GET"
+    }).done(function (response) {
+
+        $("#dataTableRT").empty(); // Vaciar la tabla antes de cargar los nuevos datos
+        var respObj = response.data;
+        for (k in respObj) { 
+
+            let justificacion = respObj[k].idJustificacionTardia ? "Justificado" : "Sin Justificar";
+            let filaHTML = `<tr data-employee-id="${respObj[k].empleado.idEmpleado}" data-justificacion="${encodeURIComponent(JSON.stringify(respObj[k].justificacion_tardia))}" data-empleado="${encodeURIComponent(JSON.stringify(respObj[k].empleado))}">
+                <td >${respObj[k].idRegistroTardia}</td>
+                <td>${respObj[k].fecha}</td>
+                <td>${respObj[k].cantMinutos}</td>
+                <td id="empleado">${respObj[k].empleado.nombre}</td>
+                <td id="justificacion">${justificacion}</td>
+                <td><input type="checkbox" class="checkbox-accion" onchange=""></td>
+            </tr>`;
+
+            let fila = $(filaHTML);
+            $("#dataTableRT").append(fila);
+        }
+        console.log(respObj)
+    }).fail(function (error) {
+        console.log(error)
+    });
+}
+
