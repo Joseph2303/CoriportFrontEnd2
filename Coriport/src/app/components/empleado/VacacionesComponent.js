e8c625bb1d66921133805048b9bb17bffa1108f1
Quintanilla1299 <suke12k17@gmail.com>
2024-04-21 17:07:01 -0600

    caso 21

--- Coriport/src/app/components/empleado/VacacionesComponent.js
+++ Coriport/src/app/components/empleado/VacacionesComponent.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
 });
 
 function cargarTabla() {
+    let usuario = JSON.parse(localStorage.getItem('identity')) 
     $.ajax({
-        url: "http://localhost:8000/api/vacaciones",
+        url: "http://localhost:8000/api/vacaciones/" + usuario.empleado.idEmpleado,
         type: "GET"
     }).done(function (response) {
         $("#dataTableVacaciones").empty(); // Vaciar la tabla antes de cargar los nuevos datos
         var respObj = response.data;
-     
         for (k in respObj) {
             let filaHTML = `<tr> 
                 <td>${respObj[k].id}</td>
