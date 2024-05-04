4521bc8a96b2309810b4970ef8b7d8d0308eb653
Quintanilla1299 <suke12k17@gmail.com>
2024-05-03 22:23:48 -0600

    caso 21

--- Coriport/src/app/Scripts/ScriptEmpleado/SolicitudVacaciones.js
+++ Coriport/src/app/Scripts/ScriptEmpleado/SolicitudVacaciones.js
@@ -1,5 +1,6 @@
-
 var tabla = document.getElementById("soliVacaciones-table");
+var updateSoli = document.getElementById("div-form-update")
+var updateBtn = document.getElementById("update");
 
 function filtrarSoliVacaciones() {
     var inputBusqueda = document.getElementById('busquedaSoliVacaciones');
@@ -24,3 +25,51 @@ function filtrarSoliVacaciones() {
         }
     }
 }
+
+tabla.addEventListener('change', function (event) {
+    let seleccionados = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (event.target.type === 'checkbox') {
+        if (seleccionados.length > 1) {
+            event.target.checked = false;
+            mostrarMensajeDeError("¡Solo puede seleccionar una solicitud a la vez! ");
+
+        } else {
+            if (seleccionados.length > 0) {
+                updateSoli.style.display = "flex";
+            } else {
+                updateSoli.style.display = "none";
+            }
+        }
+    }
+});
+
+function deseleccionarCheckboxes() {
+    document.querySelectorAll('input[type="checkbox"]:checked').forEach(function (checkbox) {
+        checkbox.checked = false;
+    });
+}
+
+
+$(document).on("click", ".checkbox-accion", function () {
+    let seleccionados = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (seleccionados.length === 1) {
+    let filaSeleccionada = seleccionados[0].closest('tr');
+    let fechInicio = filaSeleccionada.cells[1].textContent;
+    let fechFin = filaSeleccionada.cells[2].textContent;
+    document.getElementById('fechInicioUpdate').value = fechInicio;
+    document.getElementById('fechFinUpdate').value = fechFin;
+    }
+});
+
+updateBtn.addEventListener('click', function () {
+    let seleccionados = document.querySelectorAll('input[type="checkbox"]:checked');
+    let filaSeleccionada = seleccionados[0].closest('tr');
+
+    let solicitudVacaciones = {
+        id: filaSeleccionada.getAttribute('data-id'),
+        fechInicio: $("#fechInicioUpdate").val(),
+        fechFin: $("#fechFinUpdate").val()
+    }
+
+    updateSolicitud(solicitudVacaciones)
+});
--- Coriport/src/app/components/empleado/SolicitudVacacionesComponent.js
+++ Coriport/src/app/components/empleado/SolicitudVacacionesComponent.js
@@ -1,5 +1,5 @@
-function send(){
-    user = JSON.stringify(localStorage.getItem("identity"))
+function send() {
+    user = JSON.parse(localStorage.getItem("identity"))
     let soliData = {
         "fechInicio": $("#fechInicio").val(),
         "fechFin": $("#fechFin").val(),
@@ -18,13 +18,39 @@ function send(){
         data: data
     }).done(function (response) {
         console.log(response)
+        cargarTabla()
+        document.getElementById("div-form").style.display = "none";
         mostrarMensajeDeInfo("¡INFO!: " + response.message);
-
-    }).fail(function (error) {
-        console.log(error)
+    }).fail(function (xhr) {
+        console.log(xhr)
         mostrarMensajeDeError("ERROR!!: " + xhr.responseJSON.message);
     });
 }
+
+function updateSolicitud(solicitudVacaciones) {
+    mostrarMensajeDeInfo("Veficando datos....");
+
+    let updateData = {
+        "fechInicio": solicitudVacaciones.fechInicio,
+        "fechFin": solicitudVacaciones.fechFin,
+    };
+
+    let data = 'data=' + JSON.stringify(updateData);
+    $.ajax({
+        url: "http://localhost:8000/api/soliVacaciones/" + solicitudVacaciones.id,
+        type: "PUT",
+        data: data
+    }).done(function (response) {
+        cargarTabla()
+        document.getElementById("div-form-update").style.display = "none";
+        mostrarMensajeDeInfo("¡INFO!: " + response.message);
+
+    }).fail(function (xhr, status, error) {
+        console.log(xhr)
+        mostrarMensajeDeError("ERROR!!: " + xhr.responseJSON.message);
+    });
+}
+
 
 $(document).ready(function () {
     cargarTabla();
@@ -37,19 +63,25 @@ function cargarTabla() {
     }).done(function (response) {
         $("#dataTableSoliVacaciones").empty(); // Vaciar la tabla antes de cargar los nuevos datos
         var respObj = response.data;
-     
+
         for (k in respObj) {
-            let filaHTML = `<tr> 
+            let filaHTML = `<tr data-id="${respObj[k].idSoliVacaciones}"> 
                 <td>${respObj[k].fechSolicitud}</td>
                 <td>${respObj[k].fechInicio}</td>
                 <td>${respObj[k].fechFin}</td>
                 <td id="estado">${respObj[k].estado}</td>
                 <td>${respObj[k].responsableAut}</td>
                 <td>${respObj[k].descripcion}</td>
-                
+                <td><input type="checkbox" class="checkbox-accion" onchange=""></td>
+
             </tr>`;
             let fila = $(filaHTML);
-                  
+
+            if (respObj[k].estado === "Aceptado") {
+                fila.find('input[type="checkbox"]').prop('disabled', true); // Deshabilitar el checkbox
+                fila.off('click'); // Quitar todos los eventos de clic en la fila
+            }
+
             $("#dataTableSoliVacaciones").append(fila);
         }
     }).fail(function (error) {
