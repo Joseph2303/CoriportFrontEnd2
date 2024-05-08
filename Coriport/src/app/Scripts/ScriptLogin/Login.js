728c38c39d69594dc06a10fe9c44a876f0fb8777
Joseph2303 <106033045+Joseph2303@users.noreply.github.com>
2024-05-07 23:38:27 -0600

    caso 70

--- Coriport/src/app/Scripts/ScriptLogin/Login.js
+++ Coriport/src/app/Scripts/ScriptLogin/Login.js
@@ -51,3 +51,36 @@ function changeView2(view) {
 		document.getElementById('pantallaEmergente').style.display = 'none';
 	}
 	
+
+
+	const video = document.getElementById('video');
+        const canvas = document.getElementById('canvas');
+        const captureButton = document.getElementById('captureButton');
+
+        // Acceder a la cámara del dispositivo
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function(stream) {
+                video.srcObject = stream;
+            })
+            .catch(function(error) {
+                console.error('Error al acceder a la cámara:', error);
+            });
+
+        // Función para capturar una imagen de la cámara
+        captureButton.addEventListener('click', function() {
+            const context = canvas.getContext('2d');
+            // Configurar el tamaño del canvas para que coincida con el tamaño del video
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            // Dibujar el video en el canvas
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            // Convertir el contenido del canvas a una URL de imagen
+            const imageDataURL = canvas.toDataURL('image/jpeg');
+            // Ahora puedes enviar imageDataURL como el valor de la imagen en el formulario, o realizar otras operaciones con él
+            console.log(imageDataURL);
+        });
+
+        document.getElementById('myForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            // Aquí puedes agregar la lógica para enviar el formulario
+        });
