05164e09c41e71aa4ec8566c59b08259cc0fc580
Joseph2303 <106033045+Joseph2303@users.noreply.github.com>
2024-06-16 21:40:42 -0600

    CASO 202

--- Coriport/src/app/Scripts/ScriptEncargado/FaceId.js
+++ Coriport/src/app/Scripts/ScriptEncargado/FaceId.js
@@ -57,5 +57,28 @@ $(document).mouseup(function (e) {
 });
 
 
+var tabla = document.getElementById("FaceID-table");
 
+function filtrar() {
+    var inputBusqueda = document.getElementById('buscarFace');
+    var filtro = inputBusqueda.value.toUpperCase();
 
+    var filas = tabla.getElementsByTagName('tr');
+
+    for (var i = 0; i < filas.length; i++) {
+        if (filas[i].getElementsByTagName('th').length === 0) {
+            var celdas = filas[i].getElementsByTagName('td');
+            var mostrarFila = false;
+
+            for (var j = 0; j < celdas.length; j++) {
+                var textoCelda = celdas[j].textContent || celdas[j].innerText;
+                if (textoCelda.toUpperCase().indexOf(filtro) > -1) {
+                    mostrarFila = true;
+                    break;
+                }
+            }
+            
+            filas[i].style.display = mostrarFila ? '' : 'none';
+        }
+    }
+}
--- Coriport/src/app/components/encargado/FaceIdComponent.js
+++ Coriport/src/app/components/encargado/FaceIdComponent.js
@@ -1,8 +1,6 @@
-
 $(document).ready(function () {
     cargarTabla();
 });
-
 
 function cargarTabla() {
     $.ajax({
@@ -11,23 +9,47 @@ function cargarTabla() {
     }).done(function (response) {
         $("#dataTableFaceID").empty(); // Vaciar la tabla antes de cargar los nuevos datos
         var respObj = response.data;
-        console.log(respObj)
+        console.log(respObj);
         for (k in respObj) {
             let filaHTML = `<tr data-id="${respObj[k].id}" data-empleado="${encodeURIComponent(JSON.stringify(respObj[k].empleado))}"> 
                 <td>${respObj[k].id}</td>
                 <td id="empleado">${respObj[k].empleado.nombre}</td>
                 <td>${respObj[k].idEmpleado}</td>
-                <td><input type="checkbox" class="checkbox-accion" onchange=""></td>  
-
+                <td><button class="btn-eliminar" onclick="mostrarConfirmacion(${respObj[k].id})">Eliminar</button></td>
             </tr>`;
             let fila = $(filaHTML);
-
 
             $("#dataTableFaceID").append(fila);
         }
     }).fail(function (xhr, status, error) {
-        console.log(xhr)
+        console.log(xhr);
     });
 }
 
+function mostrarConfirmacion(id) {
+    $("#pantallaConfirmacion").data("id", id);
+    $("#pantallaConfirmacion").show();
+}
 
+function ocultarConfirmacion() {
+    $("#pantallaConfirmacion").hide();
+}
+
+function confirmarEliminar() {
+    var id = $("#pantallaConfirmacion").data("id");
+    eliminarFila(id);
+}
+
+function eliminarFila(id) {
+    $.ajax({
+        url: `http://localhost:8000/api/faceId/delete/${id}`,
+        type: "DELETE"
+    }).done(function (response) {
+        cargarTabla();
+        console.log(response);
+    }).fail(function (xhr, status, error) {
+        console.log(xhr);
+    }).always(function () {
+        ocultarConfirmacion();
+    });
+}
